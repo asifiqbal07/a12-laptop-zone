0d5da86adfc6f8f9145ecd34fc0daaa8ed6e6c00
asifiqbal07 <as.asif.07@gmail.com>
2022-11-26 03:48:37 +0600

    JWT Added

--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -3,18 +3,19 @@ import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider';
+import useToken from '../../../hooks/useToken';
 
 const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const { createUser, updateUser } = useContext(AuthContext);
     const [signUpError, setSignUPError] = useState('');
     const [createdUserEmail, setCreatedUserEmail] = useState('')
-    // const [token] = useToken(createdUserEmail);
+    const [token] = useToken(createdUserEmail);
     const navigate = useNavigate();
 
-    // if (token) {
-    //     navigate('/');
-    // }
+    if (token) {
+        navigate('/');
+    }
 
     const handleSignUp = (data) => {
         setSignUPError('');
@@ -50,7 +51,7 @@ const SignUp = () => {
             .then(res => res.json())
             .then(data => {
                 console.log("save user", data);
-                // setCreatedUserEmail(email);
+                setCreatedUserEmail(email);
             })
     }
     return (
